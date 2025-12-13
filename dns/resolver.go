e5271613f9305d52266376033e425f2045cb95b9
cjjdaq <cjjdaq@gmail.com>
2025-12-13 14:25:17 +0800

    Update resolver.go

--- dns/resolver.go
+++ dns/resolver.go
@@ -190,12 +190,14 @@ func (r *Resolver) ExchangeContext(ctx context.Context, m *D.Msg) (msg *D.Msg, e
 				staleAge := now.Sub(expireTime)
 				if staleAge <= optimisticStaleMax {
 					// serve stale and refresh in background
+					log.Debugln("[DNS] cache hit stale %s, refreshing in background", domain)
 					setMsgTTL(msg, uint32(1))
 					continueFetch = true
 					return
 				}
 			}
 			// too stale (or optimistic disabled): do not serve cache
+			log.Debugln("[DNS] cache expired %s, querying upstream", domain)
 			return r.exchangeWithoutCache(ctx, m)
 		}
 
